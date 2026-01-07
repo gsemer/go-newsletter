951ccb0b179a4987a83e14017db4fe2460c8ae6b
gsemer <gsemertzakis@hotmail.com>
2026-01-07 22:39:06 +0200

    connect to db and start server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,18 +2,42 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+
+	userapp "newsletter/internal/users/application"
+	userrepo "newsletter/internal/users/infrastructure/postgres"
+	transporthttp "newsletter/transport/http"
+	"newsletter/transport/http/handler"
 )
 
+var counts int64
+
 func main() {
+	conn := connectToDB()
+	if conn == nil {
+		log.Panic("Can't connect to Postgres!")
+	}
+
+	userRepo := userrepo.NewUserRepository(conn)
+	userService := userapp.NewUserService(userRepo)
+	authService := userapp.NewAuthenticationService(userRepo)
+
+	userHandler := handler.NewUserHandler(userService, authService)
+
+	app := transporthttp.App{Userhandler: *userHandler}
 
 	server := &http.Server{
 		Addr:    ":8001",
-		Handler: nil,
+		Handler: app.Routes(),
 	}
 
 	go func() {
@@ -31,4 +55,41 @@ func main() {
 
 	log.Println("Shutting down...")
 	server.Shutdown(ctx)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func connectToDB() *sql.DB {
+	dsn := os.Getenv("DSN")
+
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("Postgres not yet ready ...")
+			counts++
+		} else {
+			log.Println("Connected to Postgres!")
+			return connection
+		}
+		if counts > 10 {
+			log.Println(err)
+			return nil
+		}
+
+		log.Println("Backing off for two seconds ...")
+		time.Sleep(2 * time.Second)
+		continue
+	}
 }
