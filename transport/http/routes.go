f24a112de047118447b84306bba3f6d7c1c7a67a
gsemer <gsemertzakis@hotmail.com>
2026-01-08 16:02:56 +0200

    update routes

--- transport/http/routes.go
+++ transport/http/routes.go
@@ -8,12 +8,15 @@ import (
 	"github.com/gorilla/mux"
 
 	"newsletter/internal/infrastructure/database"
+	newsletterapp "newsletter/internal/newsletters/application"
+	newsletterrepo "newsletter/internal/newsletters/infrastructure/postgres"
 	userapp "newsletter/internal/users/application"
 	userrepo "newsletter/internal/users/infrastructure/postgres"
 )
 
 type App struct {
 	uh handler.UserHandler
+	nh handler.NewsletterHandler
 }
 
 func NewApp() *App {
@@ -23,12 +26,16 @@ func NewApp() *App {
 	}
 
 	userRepo := userrepo.NewUserRepository(conn)
+	newsletterRepo := newsletterrepo.NewNewsletterRepository(conn)
+
 	userService := userapp.NewUserService(userRepo)
 	authService := userapp.NewAuthenticationService(userRepo)
+	newsletterService := newsletterapp.NewNewsletterService(newsletterRepo)
 
 	userHandler := handler.NewUserHandler(userService, authService)
+	newsletterHandler := handler.NewNewsletterHandler(newsletterService)
 
-	return &App{uh: *userHandler}
+	return &App{uh: *userHandler, nh: *newsletterHandler}
 }
 
 func (app *App) Routes() http.Handler {
@@ -37,6 +44,10 @@ func (app *App) Routes() http.Handler {
 	userRoutes := r.PathPrefix("/users").Subrouter()
 	userRoutes.HandleFunc("/signup", app.uh.SignUp).Methods("POST")
 	userRoutes.HandleFunc("/signin", app.uh.Signin).Methods("POST")
+
+	newsletterRoutes := r.PathPrefix("/newsletters").Subrouter()
+	newsletterRoutes.Handle("", app.Validate(http.HandlerFunc(app.nh.Create))).Methods("POST")
+	newsletterRoutes.Handle("", app.Validate(http.HandlerFunc(app.nh.GetAll))).Methods("GET")
 
 	return r
 }
