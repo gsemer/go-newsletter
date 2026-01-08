c4c0ba3e280148de77a0cf98c8cbeaa6f0cc19d0
gsemer <gsemertzakis@hotmail.com>
2026-01-08 10:35:51 +0200

    init app

--- transport/http/routes.go
+++ transport/http/routes.go
@@ -1,22 +1,42 @@
 package http
 
 import (
+	"log"
 	"net/http"
 	"newsletter/transport/http/handler"
 
 	"github.com/gorilla/mux"
+
+	"newsletter/internal/infrastructure/database"
+	userapp "newsletter/internal/users/application"
+	userrepo "newsletter/internal/users/infrastructure/postgres"
 )
 
 type App struct {
-	Userhandler handler.UserHandler
+	uh handler.UserHandler
+}
+
+func NewApp() *App {
+	conn := database.ConnectWithRetry()
+	if conn == nil {
+		log.Panic("Can't connect to Postgres!")
+	}
+
+	userRepo := userrepo.NewUserRepository(conn)
+	userService := userapp.NewUserService(userRepo)
+	authService := userapp.NewAuthenticationService(userRepo)
+
+	userHandler := handler.NewUserHandler(userService, authService)
+
+	return &App{uh: *userHandler}
 }
 
 func (app *App) Routes() http.Handler {
 	r := mux.NewRouter()
 
 	userRoutes := r.PathPrefix("/users").Subrouter()
-	userRoutes.HandleFunc("/signup", app.Userhandler.SignUp).Methods("POST")
-	userRoutes.HandleFunc("/signin", app.Userhandler.Signin).Methods("POST")
+	userRoutes.HandleFunc("/signup", app.uh.SignUp).Methods("POST")
+	userRoutes.HandleFunc("/signin", app.uh.Signin).Methods("POST")
 
 	return r
 }
