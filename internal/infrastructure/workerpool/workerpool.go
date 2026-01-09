d5b0855c10406f8c4bf1219709ca816f3c2fdbf0
gsemer <gsemertzakis@hotmail.com>
2026-01-09 15:22:49 +0200

    init worker pool

---
+++ internal/infrastructure/workerpool/workerpool.go
@@ -0,0 +1,71 @@
+package workerpool
+
+import (
+	"log"
+	"log/slog"
+	"sync"
+)
+
+// Job represents a unit of work that can be processed by the worker pool.
+// Each Job must implement the Process method.
+type Job interface {
+	// Process executes the job's logic.
+	// It should return an error if the job fails.
+	Process() error
+}
+
+// WorkerPool manages a fixed number of workers that process
+// submitted jobs concurrently.
+type WorkerPool struct {
+	workers int             // number of worker goroutines
+	jobs    chan Job        // channel used to queue jobs
+	wg      *sync.WaitGroup // wait group to track job completion
+}
+
+func NewWorkerPool(workers, size int, wg *sync.WaitGroup) *WorkerPool {
+	return &WorkerPool{
+		workers: workers,
+		jobs:    make(chan Job, size),
+		wg:      wg,
+	}
+}
+
+// worker runs as a goroutine and continuously processes jobs
+// received from the job channel until the channel is closed.
+func (wp *WorkerPool) worker(i int) {
+	for job := range wp.jobs {
+		slog.Info("Worker processes job", "worker", i)
+		err := job.Process()
+		if err != nil {
+			log.Println("Error while processing the job:", err)
+			slog.Warn("Error while processing the job:", "error", err)
+		}
+		wp.wg.Done()
+	}
+}
+
+// Start launches all worker goroutines.
+// This method should be called before submitting jobs.
+func (wp *WorkerPool) Start() {
+	for i := 0; i < wp.workers; i++ {
+		go wp.worker(i)
+	}
+}
+
+// Submit adds a job to the worker pool queue.
+// It increments the WaitGroup counter before enqueuing the job.
+func (wp *WorkerPool) Submit(job Job) {
+	wp.wg.Add(1)
+	wp.jobs <- job
+}
+
+// Shutdown closes the job channel, signaling workers
+// that no more jobs will be submitted.
+func (wp *WorkerPool) Shutdown() {
+	close(wp.jobs)
+}
+
+// Wait blocks until all submitted jobs have finished processing.
+func (wp *WorkerPool) Wait() {
+	wp.wg.Wait()
+}
